091634d0a43fa9c3005304dfc1680af617851064
Rammilan Maurya <rammilan.maurya@agilisys.co.uk>
2021-11-27 23:37:15 +0000

    updated modal and CNAME

---
+++ js/modalcontroller.js
@@ -0,0 +1,632 @@
+var ModalData =[
+    {
+        "title": "Hummus",
+        "preparation": [
+            {
+                "title":"",
+                "data" : [
+                    "Preparation Time 10 minutes",
+                    "Cooking time for chickpeas 15 minutes",
+                    "Serve 2 to 3 People"
+                ]
+            }
+        ],
+        "ingredient":[
+            {
+                "title":"",
+                "data" : [
+                    "Cooked Chickpeas- 1<sup>1/2</sup> Cup",
+                    "Green Chilli - 2",
+                    "Tahini- 2 tbspn",
+                    "Garlic Gloves -2 pod",
+                    "Lemon Juice -2 tbspn",
+                    "Salt -as per your taste",
+                    "Water or aquafaba - 2 to 3 tbspn",
+                    "Olive oil- 1 tbspn"
+                ]
+            }
+        ],
+        "cooking":[
+            {
+                "title":"",
+                "data": [
+                    "Take a mixture grinder jar put cooked chickpeas and add water or aquafaba and blend it until creamy texture.",
+                    "Add Olive oil, lemon juice,tahini, garlic pod, chilli, Salt and blend for 30 sec.",
+                    "Serve hummus with a drizzle of olive oil."
+                ]
+            }
+        ],
+        "tips":[
+            {
+                "title":"",
+                "data" : [
+                    "If you will remove chickpeas cover you will get better taste.",
+                    "For flavour you can add roasted beetroot and bell pepper.",
+                    "Serve it with crackers, tortilla wrap and flat bread also.",
+                    "Store it and airtight container and refrigerate upto one week."
+                ]
+            }
+        ]
+    },
+    {
+        "title":"Spaghetti",
+        "preparation":[
+            {
+                "title":"",
+                "data" : [
+                    "Preparation Time-10 minutes",
+                    "Cooking time for spaghetti- 15-20 minutes",
+                    "Serve- 2 to 3 person"
+                ]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title":"",
+                "data":[
+                    "Boiled Spaghetti-250gm ",
+                    "Garlic pod -3 to 4",
+                    "Green Chilli -3 to 4",
+                    "Oil- 2 tbspn",  
+                    "Green Peas- 1/4 cup", 
+                    "Sweet corn- 1/4 cup",    
+                    "Carrot chopped -1/4 cup",
+                    "Dark Soya Sauce- 1 tbspn", 
+                    "Vinegar- 1/2 tbspn",   
+                    "Red Chili Sauce- 1/2 tbspn ", 
+                    "Tomato Sauce- 2 tbspn",
+                    "Salt- as per taste",
+
+                ]
+            }
+        ],
+        "cooking":[
+            {
+                "title":"",
+                "data":[
+                    "Take a frying pan, put 2 tbspn cooking oil turn flame on medium heat.",
+                    "Put garlic and green chilli stir it 30 sec and add veggies and stir until tender.",
+                    "Add dark soya sauce,vinegar,red chilli sauce, Tomato sauce,salt and stir it for 30 sec.",
+                    "Add Spaghetti and cook it 5 min on slow heat.",
+                    "Toss the spaghetti and serve it.",
+
+                ]
+            }
+        ],
+        "tips":[
+            {
+                "title":"",
+                "data":[
+                    "You can add some Cheese, Oregano, Basil on top of that.",
+                    "While cooking Spaghetti add 2 tbspn of oil on boiling water, Spaghetti will not get sticky.",
+                    "Do not over cook Spaghetti after cooking wash it with cold water and strain it.",
+                    "Add veggies as per your choice.",
+
+                ]
+            }
+        ]
+    },
+    {
+        "title" : "Sandwich",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[
+                    "Prepration- 10 min",
+                    "Cooking - 0 min",
+                    "serve- 1 person"
+                ]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[
+                    "Ripe Tomato- 2 slice",
+                    "Onion - 2 slice",
+                    "Bell pepper- 2 slice",
+                    "Green chilli chopped- 1/2 tspn",
+                    "Chat masala- 1/2 tspn",
+                    "Butter for slather- 1 tspn",
+                    "Bread- 2 slice",
+                    "Salt- as per your taste",
+                ]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[
+                    "Toast the bread.",
+                    "Place it on chopping board and slather butter on it.",
+                    "Put sliced veggies and sprinkle salt and chat masala.",
+                    "Serve it with tomato sauce or green coriander chutney"   
+            
+                ]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[
+                    "You can take veggeis as per your choice",
+                    "You can spread mayonnaise,sliced cheese & grated cheese",
+                    
+                ]
+            }
+        ]
+    },
+    {
+        "title" : "Litti Chokha",
+        "preparation":[
+            {
+                "title":"",
+                "data":[
+                    "Preparation Time 15-20 minutes",
+                    "Cooking time 40-45 minutes",
+                    "Serving 2-3 person"
+                ]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title": "For Dough",
+                "data" : [
+                    "1 Cup Wheat flour",
+                    "1/4 tsp baking powder",
+                    "2 tbspn oil or ghee",
+                    "Water for kneading"
+                ]
+            },
+            {
+                "title": "For Stuffing",
+                "data" :[
+                    "1/2 Cup roasted gram flour/sattu",
+                    "1 medium size chopped onion",
+                    "1-2 chopped green chilli",
+                    "1 tbspn lemon juice",
+                    "1/4 tbspn carom seeds",
+                    "2 tbspn oil",
+                    "Salt to taste"
+                ]
+            },
+            {
+                "title":"For Stuffing",
+                "data":[
+                    "4 medium size potatoes",
+                    "3 medium size eggplants",
+                    "2 medium size tomatoes ",
+                    "2 cloves garlic",
+                    "1-2 chopped green chilli",
+                    "2 tbsp lemon juice",
+                    "salt to taste",
+                    "1 tbspn oil"
+                ]
+            },
+            {
+                "title":"Garnish",
+                "data" :[
+                    "2 tbsp coriander leaves chopped",
+                    "1/2 cup Ghee"
+                ]
+            }
+        ],
+        "cooking":[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips":[
+            {
+                "title":"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+    {
+        "title" : "",
+        "preparation" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "ingredient" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "cooking" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ],
+        "tips" :[
+            {
+                "title" :"",
+                "data":[]
+            }
+        ]
+    },
+];
+
+$(window).load(function(){
+    $(".recipe-modal").click(function(){
+        var modalTitle = $(this).attr('modal-data');
+        var data = ModalData.find(x => x.title === modalTitle);
+        $('#SiteModalLabel').html(modalTitle);
+        $("#preparation, #ingredient, #cooking, #tipstrick").empty();
+        fillData(data.preparation, '#preparation');
+        fillData(data.ingredient, '#ingredient');
+        fillData(data.cooking, '#cooking');
+        fillData(data.tips, '#tipstrick');
+        $('#SiteModal').modal('show');
+        return false;
+    });
+    
+});
+
+function fillData(sourceData, elementId)
+{
+    sourceData.forEach(src => {     
+        $(elementId).append("<h3>"+src.title+"</h3>");
+        $(elementId).append("<ul></ul>");
+        src.data.forEach(data => {
+            $(elementId+" ul:last").append("<li>"+data+"</li>");
+        });
+    });
+}
